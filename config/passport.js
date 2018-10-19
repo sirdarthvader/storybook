ea0f97f394048de0f7ec2939457ad763dd44a732
Ashish Singh <ashishcodes4@gmail.com>
2018-10-19 18:56:57 +0530

    user fetched with google oauth

--- config/passport.js
+++ config/passport.js
@@ -2,16 +2,19 @@ const mongoose = require('mongoose');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./keys');
 
-
 module.exports = function(passport) {
-  passport.use( new GoogleStrategy({
-    clientID: keys.googleClientId,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback',
-    proxy: true
-  }, 
-    function(accessToken, profile, refreshToken, done){
-      console.log(accessToken);
-      console.log(profile);
-    }))
-}
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: keys.googleClientId,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback',
+        proxy: true,
+      },
+      (accessToken, refreshToken, profile, done) => {
+        console.log(accessToken);
+        console.log(profile);
+      }
+    )
+  );
+};
--- server.js
+++ server.js
@@ -9,10 +9,9 @@ require('./config/passport')(passport);
 //Load routes
 const auth = require('./routes/auth');
 
-
 app.get('/', (req, res) => {
   res.send('it works');
-})
+});
 
 //Use routes
 app.use('/auth', auth);
@@ -20,4 +19,4 @@ app.use('/auth', auth);
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`server started at ${port}`);
-})
+});
