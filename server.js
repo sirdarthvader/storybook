f49581fbcb4b978eca9c4528f273d95aa628af0c
Ashish Singh <ashishcodes4@gmail.com>
2018-10-19 17:41:05 +0530

    initial setup

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
-const port = process.env.PORT || 5000;
 
+app.get('/', (req, res) => {
+  res.send('it works');
+})
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`server started at ${port}`);
 })
