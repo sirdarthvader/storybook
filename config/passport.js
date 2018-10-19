1a36ea29c4d80bff3dfee58d81d6bd74c4134645
Ashish Singh <ashishcodes4@gmail.com>
2018-10-19 20:23:30 +0530

    passport authentication setup

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./keys');
+const User = mongoose.model('users')
 
 module.exports = function(passport) {
   passport.use(
@@ -12,9 +13,41 @@ module.exports = function(passport) {
         proxy: true,
       },
       (accessToken, refreshToken, profile, done) => {
-        console.log(accessToken);
-        console.log(profile);
+        // console.log(accessToken);
+        // console.log(profile);
+        const image = profile.photos[0].value.substring(0, profile.photos[0].value.indexOf('?'));
+        
+        const newUser = {
+          googleID: profile.id,
+          email: profile.emails[0].value,
+          firstName: profile.name.givenName,
+          lastName: profile.name.familyName,
+          image: image
+        }
+        User.findOne({
+          googleID: profile.id
+        })
+        .then(user => {
+          if(user) {
+            done(null, user);
+          } else {
+            new User(newUser).save()
+            .then(user => {
+              done(null, user)
+            })
+          }
+        })
       }
     )
   );
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  })
+
+  passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => {
+      done(null, user);
+    })
+  })
 };
--- server.js
+++ server.js
@@ -2,12 +2,41 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const passport = require('passport');
+const keys = require('./config/keys');
+const cookieparser = require('cookie-parser');
+const expressSession = require('express-session');
+
+app.use(cookieparser());
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUnitialized: false
+}));
+
+//Load Models
+require('./models/User');
+
+//Connect to mongoose
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(
+    keys.mongoURI,
+    {
+      useNewUrlParser: true
+    }
+  )
+  .then(() => console.log('mongoDB connected'))
+  .catch(err => console.log(err));
 
 //Passport config
 require('./config/passport')(passport);
 
 //Load routes
 const auth = require('./routes/auth');
+
+//Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', (req, res) => {
   res.send('it works');
