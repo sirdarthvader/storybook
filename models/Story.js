ca871ab888f4029ef721bf6cdf719de6af40e34a
Ashish Singh <ashishcodes4@gmail.com>
2018-10-21 04:56:58 +0530

    story model created

---
+++ models/Story.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const StorySchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  body: {
+    type: String,
+    default: 'public'
+  },
+  allowComments: {
+    type: Boolean,
+    default: true
+  },
+ comments: [{
+   commentBody: {
+     type: String,
+     required: true
+   },
+   commentDate: {
+     type: Date,
+     default: Date.now
+   },
+   commentUser: {
+     type: Schema.Types.ObjectId,
+     ref: 'users'
+   }
+ }],
+ user: {
+  type: Schema.Types.ObjectId,
+  ref: 'users'
+ },
+ date: {
+  type: Date,
+  default: Date.now
+ }
+})
+
+mongoose.model('stories', UserSchema, 'stories');
