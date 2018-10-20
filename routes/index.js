9b1ab1748dbfcf4115ba9b2491e8667b2d988fa6
Ashish Singh <ashishcodes4@gmail.com>
2018-10-20 20:19:07 +0530

    working on right nav

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('layouts/main');
+  res.render('index/welcome');
 })
 
 module.exports = router;
