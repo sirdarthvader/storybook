a4ad99486b19a7062f7444ca58e8e88a616c851c
Ashish <ashishcodes4@gmail.com>
2018-10-22 08:21:12 -0400

    working on UI

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,15 @@ const mongoose = require('mongoose');
 const Story  = mongoose.model('stories');
 const { ensureAuthenticated, ensureGuest } = require('../helpers/auth');
 
+
+
+//Index Route
 router.get('/', ensureGuest, (req, res) => {
   res.render('index/welcome');
 })
 
+
+//Dashboard Route
 router.get('/dashboard', ensureAuthenticated, (req, res) => {
   Story.find({user: req.user.id})
   .then(stories => {
@@ -15,6 +20,14 @@ router.get('/dashboard', ensureAuthenticated, (req, res) => {
       stories: stories
     });
   })
+});
+
+
+//About Route
+
+router.get('/about', (req, res) => {
+  res.render('index/about');
 })
+
 
 module.exports = router;
